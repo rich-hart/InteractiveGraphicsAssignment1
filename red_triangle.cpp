46f22a568e0619edb7d6a5169dd6cf373670ed3b
Rich Hart <richhohart@gmail.com>
2013-04-06 16:28:28 -0400

    Changeing code to render cubes

--- red_triangle.cpp
+++ red_triangle.cpp
@@ -3,64 +3,129 @@
 
 #include "include/Angel.h"
 
-const int NumPoints = 3;
+typedef Angel::vec4  color4;
+typedef Angel::vec4  point4;
 
-//--------------------------------------------------------------------------
+const int NumVertices = 36; //(6 faces)(2 triangles/face)(3 vertices/triangle)
 
+point4 points[NumVertices];
+color4 colors[NumVertices];
 
+// Vertices of a unit cube centered at origin, sides aligned with axes
+point4 vertices[8] = {
+    point4( -0.5, -0.5,  0.5, 1.0 ),
+    point4( -0.5,  0.5,  0.5, 1.0 ),
+    point4(  0.5,  0.5,  0.5, 1.0 ),
+    point4(  0.5, -0.5,  0.5, 1.0 ),
+    point4( -0.5, -0.5, -0.5, 1.0 ),
+    point4( -0.5,  0.5, -0.5, 1.0 ),
+    point4(  0.5,  0.5, -0.5, 1.0 ),
+    point4(  0.5, -0.5, -0.5, 1.0 )
+};
 
-void init( void )
+// RGBA olors
+color4 vertex_colors[8] = {
+    color4( 0.0, 0.0, 0.0, 1.0 ),  // black
+    color4( 1.0, 0.0, 0.0, 1.0 ),  // red
+    color4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+    color4( 0.0, 1.0, 0.0, 1.0 ),  // green
+    color4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+    color4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
+    color4( 1.0, 1.0, 1.0, 1.0 ),  // white
+    color4( 0.0, 1.0, 1.0, 1.0 )   // cyan
+};
+
+//----------------------------------------------------------------------------
+
+// quad generates two triangles for each face and assigns colors
+//    to the vertices
+int Index = 0;
+void
+quad( int a, int b, int c, int d )
 {
-    // Specifiy the vertices for a triangle
+    colors[Index] = vertex_colors[a]; points[Index] = vertices[a]; Index++;
+    colors[Index] = vertex_colors[b]; points[Index] = vertices[b]; Index++;
+    colors[Index] = vertex_colors[c]; points[Index] = vertices[c]; Index++;
+    colors[Index] = vertex_colors[a]; points[Index] = vertices[a]; Index++;
+    colors[Index] = vertex_colors[c]; points[Index] = vertices[c]; Index++;
+    colors[Index] = vertex_colors[d]; points[Index] = vertices[d]; Index++;
+}
 
-    vec2 triangle_vertices[3] = {
-        vec2( -0.75, -0.75 ), vec2( 0.0, 0.75 ), vec2( 0.75, -0.75 )
-    };
+//----------------------------------------------------------------------------
+
+// generate 12 triangles: 36 vertices and 36 colors
+void
+colorcube()
+{
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+}
+
+//----------------------------------------------------------------------------
+
+// OpenGL initialization
+void
+init()
+{
+    colorcube();
 	
-
-
     // Create a vertex array object
     GLuint vao;
     glGenVertexArraysAPPLE( 1, &vao );
     glBindVertexArrayAPPLE( vao );
-    
-
+	
     // Create and initialize a buffer object
     GLuint buffer;
     glGenBuffers( 1, &buffer );
     glBindBuffer( GL_ARRAY_BUFFER, buffer );
-    glBufferData( GL_ARRAY_BUFFER, sizeof(triangle_vertices), triangle_vertices, GL_STATIC_DRAW );
-
+    glBufferData( GL_ARRAY_BUFFER, sizeof(points) + sizeof(colors),
+				 NULL, GL_STATIC_DRAW );
+    glBufferSubData( GL_ARRAY_BUFFER, 0, sizeof(points), points );
+    glBufferSubData( GL_ARRAY_BUFFER, sizeof(points), sizeof(colors), colors );
+	
     // Load shaders and use the resulting shader program
-    GLuint program = InitShader( "vshader21.glsl", "fshader21.glsl" );
+    GLuint program = InitShader( "vshader31.glsl", "fshader31.glsl" );
     glUseProgram( program );
-
-    // Initialize the vertex position attribute from the vertex shader
-    GLuint loc = glGetAttribLocation( program, "vPosition" );
-    glEnableVertexAttribArray( loc );
-    glVertexAttribPointer( loc, 2, GL_FLOAT, GL_FALSE, 0,
-                           BUFFER_OFFSET(0) );
-
-    glClearColor( 0.0, 0.0, 0.0, 0.0 ); // white background
+	
+    // set up vertex arrays
+    GLuint vPosition = glGetAttribLocation( program, "vPosition" );
+    glEnableVertexAttribArray( vPosition );
+    glVertexAttribPointer( vPosition, 4, GL_FLOAT, GL_FALSE, 0,
+						  BUFFER_OFFSET(0) );
+	
+    GLuint vColor = glGetAttribLocation( program, "vColor" ); 
+    glEnableVertexAttribArray( vColor );
+    glVertexAttribPointer( vColor, 4, GL_FLOAT, GL_FALSE, 0,
+						  BUFFER_OFFSET(sizeof(points)) );
+	
+    glEnable( GL_DEPTH_TEST );
+    glClearColor( 1.0, 1.0, 1.0, 1.0 ); 
 }
 
 //----------------------------------------------------------------------------
 
-void display( void )
+void
+display( void )
 {
-    glClear( GL_COLOR_BUFFER_BIT );     // clear the window
-    glDrawArrays( GL_TRIANGLES, 0, NumPoints );    // draw the points
-    glFlush();
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    glDrawArrays( GL_TRIANGLES, 0, NumVertices );
+    glutSwapBuffers();
 }
 
 //----------------------------------------------------------------------------
 
-void keyboard( unsigned char key, int x, int y )
+void
+keyboard( unsigned char key, int x, int y )
 {
-    switch ( key ) {
-    case 033:
-        exit( EXIT_SUCCESS );
-        break;
+    switch( key ) {
+		case 033:  // Escape key
+		case 'q': case 'Q':
+			exit( EXIT_SUCCESS );
+			break;
     }
 }
 
@@ -70,20 +135,15 @@ int
 main( int argc, char **argv )
 {
     glutInit( &argc, argv );
-    glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGBA );
+    glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH );
     glutInitWindowSize( 512, 512 );
-    glutInitWindowPosition(100, 100);
-
-    glutCreateWindow( "Red Triangle" );
-    
-   
+    glutCreateWindow( "Color Cube" );
+	
     init();
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	
     glutDisplayFunc( display );
     glutKeyboardFunc( keyboard );
-
+	
     glutMainLoop();
-    glClear(GL_COLOR_BUFFER_BIT);
-    glutSwapBuffers();
     return 0;
 }
